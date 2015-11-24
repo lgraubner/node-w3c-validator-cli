ce66de21d8cd384c90273564a18c2f9973732b9e
lgraubner <graubnla@gmail.com>
2015-11-24 13:55:18 +0100

    refactored

--- test/cli.js
+++ test/cli.js
@@ -2,70 +2,23 @@ var should = require("chai").should();
 var exec = require("child_process").exec;
 
 describe("$ w3c-validator invalid", function() {
-    var _error;
-    var _stdout;
-    var _stderr;
-
-    before(function(done) {
-        var cmd = exec("node w3c-validator.js illegal", function(error, stdout, stderr) {
-            _error = error;
-            _stdout = stdout;
-            _stderr = stderr;
-            done();
-        });
-    });
-
-    it("should fail because of invalid url", function() {
-        _stderr.should.contain("could not be found");
-    });
-
-    it("should exit with error code '1'", function() {
-        _error.code.should.equal(1);
-    });
+    it("should fail because of invalid url");
+    it("should exit with error code '1'");
 });
 
-describe("$ w3c-validator abc.xyz", function() {
-    this.timeout(300000);
-
-    var _error;
-    var _stdout;
-    var _stderr;
-
-    before(function(done) {
-        var cmd = exec("node w3c-validator.js abc.xyz", function(error, stdout, stderr) {
-            _error = error;
-            _stdout = stdout;
-            _stderr = stderr;
-            done();
-        });
-    });
-
-    it("should not throw any errors", function() {
-        should.equal(_error, null);
-    });
-
-    it("should return data", function() {
-        _stdout.should.not.be.empty;
-    });
+describe("$ w3c-validator 127.0.0.1", function() {
+    it("should not throw any errors");
+    it("should return success message");
 });
 
-describe("$ w3c-validator https://abc.xyz", function() {
-    this.timeout(300000);
+describe("$ w3c-validator --log 127.0.0.1", function() {
+    it("should create a text file");
+});
 
-    var _error;
-    var _stdout;
-    var _stderr;
+describe("$ w3c-validator --query 127.0.0.1", function() {
+    it("should include links with query parameters");
+});
 
-    before(function(done) {
-        var cmd = exec("node w3c-validator.js https://abc.xyz", function(error, stdout, stderr) {
-            _error = error;
-            _stdout = stdout;
-            _stderr = stderr;
-            done();
-        });
-    });
-
-    it("should work with protocol and not throw any errors", function() {
-        should.equal(_error, null);
-    });
+describe("$ w3c-validator --verbose 127.0.0.1", function() {
+    it("should output additional error information");
 });
